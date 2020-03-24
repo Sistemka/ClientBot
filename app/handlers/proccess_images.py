acbbedf926c5ae01d9ac90e39cb1034cb84638a5
unknown <dr.interned@gmail.com>
2020-03-24 21:29:05 +0300

    output prettified

--- app/handlers/proccess_images.py
+++ app/handlers/proccess_images.py
@@ -1,7 +1,10 @@
 import os
+import re
+import io
 import uuid
 import shutil
 import logging
+import requests
 from pathlib import Path
 
 from telegram import ChatAction, InputMediaPhoto, InputMedia
@@ -78,17 +81,25 @@ def full_process_image(update, context):
         )
 
     for image_pair in images_pairs:
-        context.bot.send_message(
-            text=open(Path(files_to_return_dir, image_pair[1]), 'r').read(),
-            chat_id=update.effective_user.id,
-        )
-        # context.bot.send_media_group(
-        #     chat_id=update.effective_user.id,
-        #     media=[
-        #         InputMediaPhoto(open(Path(files_to_return_dir, image_pair[0]), 'rb')),
-        #         InputMediaPhoto(open(Path(files_to_return_dir, image_pair[2]), 'rb'))
-        #     ]
-        # )
+        try:
+            link = open(Path(files_to_return_dir, image_pair[1]), 'r').readline().replace('\n','')
+            text = requests.get(link).text
+            pic_link = re.findall(r'https:\/\/cdn\.ennergiia\.com\/new-images\/ennergiia-catalog\/\w+\/\w+\/\w+\.jpg', text)[0]
+            pic = io.BytesIO(requests.get(pic_link).content)
+            context.bot.send_media_group(
+                chat_id=update.effective_user.id,
+                media=[
+                    InputMediaPhoto(open(Path(files_to_return_dir, image_pair[0]), 'rb'),
+                                    caption = open(Path(files_to_return_dir, image_pair[1]), 'r').read()),
+                    InputMediaPhoto(pic)
+                ]
+            )
+        except:
+            context.bot.send_message(
+                text=open(Path(files_to_return_dir, image_pair[1]), 'r').read(),
+                chat_id=update.effective_user.id,
+            )
+
     shutil.rmtree(files_to_return_dir)
 
 
